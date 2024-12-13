99f1293e0a9dc6faf14f6f9d465465f5d7482f3a
zhongshao <zhongshao0122@gmail.com>
2024-12-13 17:31:05 +0800

    [question] 警告Warning，还未处理

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Hello, World!'
 
-
+# 仅在开发环境中使用
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)  # 仅用于开发环境
